163164683c536f708c7caef2c95fbad6643438eb
RossPin <dorpetz@hotmail.com>
2020-09-03 10:30:20 +1200

    specify path to  .env file

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') })
 const server = require('./server')
 
 const port = process.env.PORT || 3000
