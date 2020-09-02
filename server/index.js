a50e27eb274a15597ab95da61764a9af0dacd7e8
RossPin <36139364+RossPin@users.noreply.github.com>
2020-09-02 15:11:45 +1200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const server = require('./server')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 server.listen(port, () => {
   // eslint-disable-next-line no-console
